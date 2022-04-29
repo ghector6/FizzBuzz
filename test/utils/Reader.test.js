0941b7aa3f43ea8dc7dc7e59552fb3d505f89527
ghector6 <ghectormus@gmail.com>
2022-04-29 01:26:54 -0500

    Paso 12.5: Reparando la prueba para que ahora si pase en el github actions

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,7 +3,7 @@ const Reader = require("./../../lib/utils/Reader")
 describe("Test To see if it is reading", () => {
     test("if it is catching objects from the path", () => {
         const explorers = Reader.readJsonFile("./explorers.json")
-        expect(explorers.length).toBe(0)
+        expect(explorers.length).not.toBe(0)
 
     })
 })
