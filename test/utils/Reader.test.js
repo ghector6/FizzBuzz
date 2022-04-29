436b9ed4ad13cacb6286fcc88507cee3c714741c
ghector6 <ghectormus@gmail.com>
2022-04-29 00:53:12 -0500

    Paso 7: Agregando prueba para el Reader, midiendo su length esperando que !== 0 indicando que está leyendo mas objectos

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require("./../../lib/utils/Reader")
+
+describe("Test To see if it is reading", () => {
+    test("if it is catching objects from the path", () => {
+        const explorers = Reader.readJsonFile("./explorers.json")
+        expect(explorers.length).not.toBe(0)
+
+    })
+})
