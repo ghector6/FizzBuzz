e9ecdc2d8946f744836de5c5d0931df6a5a9da93
ghector6 <ghectormus@gmail.com>
2022-04-29 22:45:34 -0500

    Terminando practica con linter, ver si puedo agregar prueba a service

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,14 +1,14 @@
-const ExplorerController = require("./../../lib/controllers/ExplorerController")
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Test to see if my methods at ExplorerController are working", () => {
 
     test("if the getExplorerByMission is getting the filter list", () => {
-        const explorers = ExplorerController.getExplorerByMission("node")
-        expect(explorers.length).toBe(10)
-    })
+        const explorers = ExplorerController.getExplorerByMission("node");
+        expect(explorers.length).toBe(10);
+    });
 
     test("if I get the userName of my explorer with map", () => {
-        const explorers = ExplorerController.getExplorersUsernamesByMission("node")
-        expect(explorers[0]).toBe("ajolonauta1")
-    })
-})
+        const explorers = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorers[0]).toBe("ajolonauta1");
+    });
+});
